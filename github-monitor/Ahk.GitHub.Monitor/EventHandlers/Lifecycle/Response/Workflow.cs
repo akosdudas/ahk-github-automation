1ab019c07413e0f4db0fbc1a77ca1ea069ced680
Gergő Radeczki <g.radeczki@gmail.com>
2022-03-05 16:22:37 +0100

    Added unimplemented Response objects

---
+++ github-monitor/Ahk.GitHub.Monitor/EventHandlers/Lifecycle/Response/Workflow.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahk.GitHub.Monitor.EventHandlers.Lifecycle.Response
+{
+    public class Workflow
+    {
+        public Workflow()
+        {
+        }
+
+        public Workflow(string badgeUrl, DateTimeOffset createdAt, string htmlUrl, long id, string name, string nodeId, string path, string state, DateTimeOffset updatedAt, string url)
+        {
+            this.BadgeUrl = badgeUrl;
+            this.CreatedAt = createdAt;
+            this.HtmlUrl = htmlUrl;
+            this.Id = id;
+            this.Name = name;
+            this.NodeId = nodeId;
+            this.Path = path;
+            this.State = state;
+            this.UpdatedAt = updatedAt;
+            this.Url = url;
+        }
+
+        public string BadgeUrl { get; protected set; }
+        public DateTimeOffset CreatedAt { get; protected set; }
+        public string HtmlUrl { get; protected set; }
+        public long Id { get; protected set; }
+        public string Name { get; protected set; }
+        public string NodeId { get; protected set; }
+        public string Path { get; protected set; }
+        public string State { get; protected set; }
+        public DateTimeOffset UpdatedAt { get; protected set; }
+        public string Url { get; protected set; }
+    }
+}
