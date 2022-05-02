53628ddbf4198b789c0eb8b48014c7e0204b5b35
Gergő Radeczki <g.radeczki@gmail.com>
2022-05-02 04:12:19 +0200

    Simplified code

--- lifecycle-management/Ahk.Lifecycle.Management.DAL/CosmosDbRepository.cs
+++ lifecycle-management/Ahk.Lifecycle.Management.DAL/CosmosDbRepository.cs
@@ -1,6 +1,10 @@
 using System.Collections;
+using System.Linq;
+using Ahk.Lifecycle.Management.DAL.Dto;
 using Ahk.Lifecycle.Management.DAL.Entities;
 using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Newtonsoft.Json;
 
 namespace Ahk.Lifecycle.Management.DAL
 {
@@ -8,22 +12,16 @@ namespace Ahk.Lifecycle.Management.DAL
     {
         private const string databaseName = "ahk";
         private const string eventsContainerName = "events";
-        private const string eventsContainerPartitionKey = "/eventType";
-        private const string statisticsContainerName = "statistics";
-        private const string statisticsContainerPartitionKey = "/id";
+        private const string eventsContainerPartitionKey = "/id";
 
         private readonly Database database;
         private readonly Container eventsContainer;
-        private readonly Container statisticsContainer;
         private volatile bool created;
-
-        private static readonly SemaphoreSlim lockObject = new SemaphoreSlim(1, 1);
 
         public CosmosDbRepository(CosmosClient client)
         {
             this.database = client.GetDatabase(databaseName);
             this.eventsContainer = database.GetContainer(eventsContainerName);
-            this.statisticsContainer = database.GetContainer(statisticsContainerName);
         }
 
         private async Task ensureCreated()
@@ -33,160 +31,42 @@ namespace Ahk.Lifecycle.Management.DAL
 
             await this.database.Client.CreateDatabaseIfNotExistsAsync(databaseName);
             await this.database.CreateContainerIfNotExistsAsync(new ContainerProperties() { Id = eventsContainerName, PartitionKeyPath = eventsContainerPartitionKey });
-            await this.database.CreateContainerIfNotExistsAsync(new ContainerProperties() { Id = statisticsContainerName, PartitionKeyPath = statisticsContainerPartitionKey });
 
             created = true;
         }
 
-        private static RepositoryStatistics createRepositoryStatistics(string repository = "", string username = "", string branchName = "", string action = "", string[] assignees = null!, string neptun = "", string conclusion = "", bool isGraded = false, int workflowRunCount = 0, int eventCount = 0, LifecycleEvent firstEvent = null!, LifecycleEvent lastEvent = null!)
+        public async Task<ICollection<Statistics>> GetRepositories(string repository = "")
         {
-            return new RepositoryStatistics()
-            {
-                Id = repository,
-                Username = username,
-                BranchName = branchName,
-                Action = action,
-                Assignees = assignees,
-                Neptun = neptun,
-                Conclusion = conclusion,
-                IsGraded = isGraded,
-                WorkflowRunCount = workflowRunCount,
-                EventCount = eventCount,
-                FirstEventType = firstEvent.EventType,
-                FirstEventDate = firstEvent.Timestamp,
-                FirstEventId = firstEvent.Id,
-                LastEventType = lastEvent.EventType,
-                LastEventDate = lastEvent.Timestamp,
-                LastEventId = lastEvent.Id,
-            };
-        }
+            if (string.IsNullOrEmpty(repository))
+                return null;
 
-        public Task<ICollection> GetMany(string repository = "", string username = "", int page = 1, int limit = 10) => throw new NotImplementedException();
-
-
-        public async Task Insert(RepositoryCreateEvent data)
-        {
-            await this.ensureCreated();
-            await eventsContainer.CreateItemAsync<RepositoryCreateEvent>(data, new PartitionKey(data.EventType));
-            await this.Upsert(createRepositoryStatistics(
-                repository: data.Repository,
-                username: data.Username,
-                eventCount: 1,
-                firstEvent: data,
-                lastEvent: data));
-        }
-
-        public async Task Insert(BranchCreateEvent data)
-        {
-            await this.ensureCreated();
-            await eventsContainer.CreateItemAsync<BranchCreateEvent>(data, new PartitionKey(data.EventType));
-            await this.Upsert(createRepositoryStatistics(
-                repository: data.Repository,
-                username: data.Username,
-                branchName: data.Branch,
-                eventCount: 1,
-                firstEvent: data,
-                lastEvent: data));
-        }
-
-        public async Task Insert(PullRequestEvent data)
-        {
-            await this.ensureCreated();
-            await eventsContainer.CreateItemAsync<PullRequestEvent>(data, new PartitionKey(data.EventType));
-            await this.Upsert(createRepositoryStatistics(
-                repository: data.Repository,
-                username: data.Username,
-                action: data.Action,
-                assignees: data.Assignees,
-                neptun: data.Neptun,
-                eventCount: 1,
-                firstEvent: data,
-                lastEvent: data));
-        }
-
-        public async Task Insert(WorkflowRunEvent data)
-        {
-            await this.ensureCreated();
-            await eventsContainer.CreateItemAsync<WorkflowRunEvent>(data, new PartitionKey(data.EventType));
-            await this.Upsert(createRepositoryStatistics(
-                repository: data.Repository,
-                username: data.Username,
-                conclusion: data.Conclusion,
-                eventCount: 1,
-                firstEvent: data,
-                lastEvent: data));
-        }
-
-        public async Task Insert(SetGradeEvent data)
-        {
-            await this.ensureCreated();
-            await eventsContainer.CreateItemAsync<SetGradeEvent>(data, new PartitionKey(data.EventType));
-            await this.Upsert(createRepositoryStatistics(
-                repository: data.Repository,
-                username: data.Username,
-                isGraded: true,
-                eventCount: 1,
-                firstEvent: data,
-                lastEvent: data));
-        }
-
-        private static string getCorrectValue(string newer, string older) => (string.IsNullOrEmpty(newer) && !string.IsNullOrEmpty(older)) ? older : newer;
-
-        public async Task Upsert(RepositoryStatistics data)
-        {
             await this.ensureCreated();
 
-            await lockObject.WaitAsync();
+            var data = new List<LifecycleEvent>();
 
-            try
-            {
-                await statisticsContainer.CreateItemAsync(data, new PartitionKey(data.Id));
-            }
-            catch (Exception)
-            {
-                var result = await statisticsContainer.ReadItemAsync<RepositoryStatistics>(data.Id, new PartitionKey(data.Id));
+            using var iter = eventsContainer
+                .GetItemLinqQueryable<LifecycleEvent>(allowSynchronousQueryExecution: true)
+                .Where(o => o.Repository.Contains(repository))
+                .ToFeedIterator();
 
-                data.BranchName = getCorrectValue(data.BranchName, result.Resource.BranchName);
-                data.Action = getCorrectValue(data.Action, result.Resource.Action);
+            while (iter.HasMoreResults)
+                data.AddRange(await iter.ReadNextAsync());
 
-                if(result.Resource.Assignees != null && result.Resource.Assignees.Length > 0 && (data.Assignees == null))
-                    data.Assignees = result.Resource.Assignees;
+            var results = data
+                    .GroupBy(o => o.Repository)
+                    .Select(o => new Statistics(
+                        repository: o.Key,
+                        count: o.Count(),
+                        events: o.ToList()))
+                    .ToList();
 
-                data.Neptun = getCorrectValue(data.Neptun, result.Resource.Neptun);
+            return results;
+        }
 
-                if(result.Resource.WorkflowRunCount > 0)
-                    data.WorkflowRunCount = result.Resource.WorkflowRunCount;
-
-                if (!string.IsNullOrEmpty(data.Conclusion))
-                    data.WorkflowRunCount++;
-
-                data.Conclusion = getCorrectValue(data.Conclusion, result.Resource.Conclusion);
-
-                if (result.Resource.IsGraded)
-                    data.IsGraded = true;
-
-                data.EventCount = result.Resource.EventCount + 1;
-
-                if (data.LastEventDate < result.Resource.LastEventDate)
-                {
-                    data.LastEventId = result.Resource.LastEventId;
-                    data.LastEventType = result.Resource.LastEventType;
-                    data.LastEventDate = result.Resource.LastEventDate;
-                }
-
-                if (data.FirstEventDate > result.Resource.FirstEventDate)
-                {
-                    data.FirstEventId = result.Resource.FirstEventId;
-                    data.FirstEventType = result.Resource.FirstEventType;
-                    data.FirstEventDate = result.Resource.FirstEventDate;
-                }
-
-                await statisticsContainer.UpsertItemAsync(data, new PartitionKey(data.Id));
-            }
-            finally
-            {
-                lockObject.Release();
-            }
+        public async Task Insert<T>(T data) where T : LifecycleEvent
+        {
+            await this.ensureCreated();
+            await eventsContainer.CreateItemAsync<T>(data, new PartitionKey(data.Id));
         }
     }
 }
--- lifecycle-management/Ahk.Lifecycle.Management.DAL/IRepository.cs
+++ lifecycle-management/Ahk.Lifecycle.Management.DAL/IRepository.cs
@@ -1,17 +1,11 @@
-using System.Collections;
-using System.Threading.Tasks;
+using Ahk.Lifecycle.Management.DAL.Dto;
 using Ahk.Lifecycle.Management.DAL.Entities;
 
 namespace Ahk.Lifecycle.Management.DAL
 {
     public interface IRepository
     {
-        Task Insert(RepositoryCreateEvent data);
-        Task Insert(BranchCreateEvent data);
-        Task Insert(PullRequestEvent data);
-        Task Insert(WorkflowRunEvent data);
-        Task Insert(SetGradeEvent data);
-        Task Upsert(RepositoryStatistics data);
-        Task<ICollection> GetMany(string repository = "", string username = "", int page = 1, int limit = 10);
+        Task Insert<T>(T data) where T : LifecycleEvent;
+        Task<ICollection<Statistics>> GetRepositories(string repository = "");
     }
 }
