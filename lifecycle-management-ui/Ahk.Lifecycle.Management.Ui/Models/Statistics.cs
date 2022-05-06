e391d4e876f8f627b4c135f792f08492d7ab7235
Gergő Radeczki <g.radeczki@gmail.com>
2022-05-06 02:23:04 +0200

    Added early implementation of Blazor UI
    - Blazor WASM ui with MudBlazor
    - Simple interface to call backend
    - Paginated result displaying
    - Error handling

---
+++ lifecycle-management-ui/Ahk.Lifecycle.Management.Ui/Models/Statistics.cs
@@ -0,0 +1,17 @@
+namespace Ahk.Lifecycle.Management.Ui.Models
+{
+    public class Statistics
+    {
+        public Statistics(int count, string repository, IReadOnlyCollection<LifecycleEvent> events)
+        {
+            this.Count = count;
+            this.Repository = repository;
+            this.Events = events;
+        }
+
+        public int Count { get; }
+        public string Repository { get; }
+        public IReadOnlyCollection<LifecycleEvent> Events { get; }
+        public bool ShowDetails { get; set; }
+    }
+}
